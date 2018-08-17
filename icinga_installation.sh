fea51524ec41ec561d5624956b86887652ad8293
VIKRAM PRATAP SINGH <99vikrampratap@gmail.com>
2018-08-17 14:19:55 +0530

    Update icinga_installation.sh

--- icinga_installation.sh
+++ icinga_installation.sh
@@ -1,24 +1,25 @@
 #!/bin/sh
-sudo apt-get update -y
+sudo apt-get update 
 sudo apt-get install apache2 -y
 sudo apt-get install php7.0 libapache2-mod-php7.0 php7.0-gd php7.0-intl php7.0-xml php7.0-ldap php7.0-mysql php7.0-pgsql php-imagick zip unzip icingaweb2 icingaweb2-module-monitoring icingaweb2-module-doc mariadb-client mariadb-server  -y
 sudo apt-get -f install -y
 wget -O - http://packages.icinga.org/icinga.key | sudo apt-key add -
 sudo add-apt-repository 'deb http://packages.icinga.org/ubuntu icinga-xenial main'
+
 sudo apt-get update
+sudo apt-get install icinga2 nagios-plugins icinga2-ido-mysql -y
+
 sudo systemctl start apache2.service
 sudo systemctl enable apache2.service
 sudo systemctl start mysql.service
 sudo systemctl enable mysql.service
-
-sudo apt-get install icinga2 nagios-plugins -y
 sudo systemctl start icinga2.service
 sudo systemctl enable icinga2.service
-sudo apt-get install icinga2-ido-mysql -y
 
 sudo groupadd icingacmd
 sudo usermod -a -G icingacmd www-data
 id www-data
+
 sudo icingacli setup config webserver apache --document-root /usr/share/icingaweb2/public
 sudo systemctl restart apache2.service
 
@@ -48,7 +49,7 @@ sudo mysql -u root -p${pass} icinga < /usr/share/icinga2-ido-mysql/schema/mysql.
 sudo mysql -u root -p${pass} -e "CREATE DATABASE icingaweb2;"
 mysql -u root -p${pass} icingaweb2 < /usr/share/icingaweb2/etc/schema/mysql.schema.sql
 
-sudo echo "date.timezone = America/Los_Angeles" >>  /etc/php/7.0/apache2/php.ini
+sudo echo "date.timezone = Asia/Kolkata" >>  /etc/php/7.0/apache2/php.ini
 sudo systemctl restart apache2.service
 
 sudo vi /etc/icinga2/features-available/ido-mysql.conf
@@ -62,21 +63,22 @@ wget http://downloads.sourceforge.net/project/pnp4nagios/PNP-0.6/pnp4nagios-0.6.
 tar -zxf pnp4nagios-0.6.26.tar.gz
 cd pnp4nagios-0.6.26/
 ./configure --with-nagios-user=naemon --with-nagios-group=naemon
-useradd naemon
-groupadd naemon
-usermod -a -G naemon naemon
-
 make all
 make install
 make install-webconf
 make install-config
 make install-init
 
+useradd naemon
+groupadd naemon
+usermod -a -G naemon naemon
+
 update-rc.d npcd defaults
 service npcd start
 
 icinga2 node wizard
 sudo systemctl restart icinga2
+
 sudo mkdir /etc/icinga2/zones.d/master/
 cp /etc/icinga2/conf.d/templates.conf /etc/icinga2/zones.d/master/
 cp /etc/icinga2/conf.d/commands.conf /etc/icinga2/zones.d/master/
@@ -108,19 +110,6 @@ apply Service "mem" {
   command_endpoint = host.vars.client_endpoint
   assign where host.vars.client_endpoint
 }
-apply Service "disk" {
-  import "generic-service"
-  check_command = "disk"
-  command_endpoint = host.vars.client_endpoint
-  assign where host.vars.client_endpoint
-}
-
-apply Service "mem" {
-  import "generic-service"
-  check_command = "mem"
-  command_endpoint = host.vars.client_endpoint
-  assign where host.vars.client_endpoint
-}
 apply Service "procs" {
   import "generic-service"
   check_command = "procs"
@@ -128,9 +117,11 @@ apply Service "procs" {
   assign where host.vars.client_endpoint
 }" > /etc/icinga2/zones.d/master/services.conf
 
+sudo systemctl restart icinga2
 sudo icinga2 daemon --validate
-sudo vim /usr/share/icinga2/include/plugins-contrib.d/operating-system.conf
 sudo icingacli setup token create
+sudo vim /usr/share/icinga2/include/plugins-contrib.d/operating-system.conf
+sudo df -h
 
 #sudo vi /etc/icinga2/features-available/ido-mysql.conf
 #user = "icinga"
