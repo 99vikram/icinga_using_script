399350a8d681214e091cad7fcd6e267445f32810
VIKRAM PRATAP SINGH <99vikrampratap@gmail.com>
2018-08-17 14:38:37 +0530

    Update icinga_client_setup.sh

--- icinga_client_setup.sh
+++ icinga_client_setup.sh
@@ -1,7 +1,13 @@
 wget -O - http://packages.icinga.org/icinga.key | sudo apt-key add -
-add-apt-repository 'deb http://packages.icinga.org/ubuntu icinga-xenial main'
-apt-get install icinga2 nagios-plugins -y
+sudo add-apt-repository 'deb http://packages.icinga.org/ubuntu icinga-xenial main'
+sudo apt-get update
+sudo apt-get install icinga2 nagios-plugins -y
+
 sudo systemctl start icinga2
 sudo systemctl enable icinga2
 sudo systemctl status icinga2
 icinga2 node wizard
+
+sudo ufw allow 5665
+sudo systemctl restart icinga2
+netstat | grep :5665
