dab51ad8592c4a383cc5112917e0d7aa8a0e5cbc
VIKRAM PRATAP SINGH <99vikrampratap@gmail.com>
2018-08-17 14:40:52 +0530

    Update icinga_client_setup.sh

--- icinga_client_setup.sh
+++ icinga_client_setup.sh
@@ -1,3 +1,4 @@
+cd /tmp
 wget -O - http://packages.icinga.org/icinga.key | sudo apt-key add -
 sudo add-apt-repository 'deb http://packages.icinga.org/ubuntu icinga-xenial main'
 sudo apt-get update
